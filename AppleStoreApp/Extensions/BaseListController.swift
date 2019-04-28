82e2c72321370d601bb52ab0f4952fbabeb92e1e
Alex Grinberg <alexxgrin@gmail.com>
2019-04-28 16:55:45 +0300

    Pods

---
+++ AppleStoreApp/Extensions/BaseListController.swift
@@ -0,0 +1,21 @@
+//
+//  BaseListController.swift
+//  AppleStoreApp
+//
+//  Created by Alex Grinberg on 28/04/2019.
+//  Copyright © 2019 Alex Grinberg. All rights reserved.
+//
+
+import UIKit
+
+class BaseListController: UICollectionViewController {
+    
+    init() {
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
